0a7d9df5a870249d4b09767b7d07e5716db6b121
James Railton <jamesrailton23@gmail.com>
2021-09-10 16:42:04 +0100

    Add project files.

---
+++ Basic Calculator/default.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="Basic_Calculator._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: x-large;
+            text-decoration: underline;
+        }
+        .auto-style2 {
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style2">
+            <span class="auto-style1"><strong>basic calculator</strong></span><br />
+            <br />
+&nbsp;Value one =
+            <asp:TextBox ID="value_one_TB" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Value two =&nbsp;
+            <asp:TextBox ID="value_two_TB" runat="server"></asp:TextBox>
+            <br />
+            <br />
+&nbsp;
+            <asp:Button ID="button_add" runat="server" BorderStyle="Solid" OnClick="button_add_Click" Text="  +  " />
+&nbsp;<asp:Button ID="button_minus" runat="server" BorderStyle="Solid" OnClick="button_minus_Click" Text="  -  " />
+            <br />
+&nbsp;
+            <asp:Button ID="button_times" runat="server" BorderStyle="Solid" OnClick="button_times_Click" style="width: 37px" Text="  x  " />
+&nbsp;<asp:Button ID="button_div" runat="server" BorderStyle="Solid" OnClick="button_div_Click" Text="  ÷  " />
+            <br />
+&nbsp;<br />
+&nbsp;&nbsp;
+            <asp:Label ID="label_value_one" runat="server"></asp:Label>
+&nbsp;<asp:Label ID="label_operator" runat="server"></asp:Label>
+&nbsp;<asp:Label ID="label_value_two" runat="server"></asp:Label>
+&nbsp;<asp:Label ID="label_eqs" runat="server"></asp:Label>
+&nbsp;<asp:Label ID="label_result" runat="server"></asp:Label>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Basic Calculator/default.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Basic_Calculator
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void button_add_Click(object sender, EventArgs e)
+        {
+
+            double value_one = double.Parse(value_one_TB.Text);
+            double value_two = double.Parse(value_two_TB.Text);
+           
+            double result = value_one + value_two;
+
+            label_value_one.Text = value_one.ToString();
+            label_value_two.Text = value_two.ToString();
+            label_eqs.Text = " = ";
+
+            label_operator.Text = "  +  ";
+            
+            label_result.Text = result.ToString();
+
+        }
+
+        protected void button_minus_Click(object sender, EventArgs e)
+        {
+
+            double value_one = double.Parse(value_one_TB.Text);
+            double value_two = double.Parse(value_two_TB.Text);
+
+            double result = value_one - value_two;
+
+            label_value_one.Text = value_one.ToString();
+            label_value_two.Text = value_two.ToString();
+            label_eqs.Text = " = ";
+
+            label_operator.Text = "  -  ";
+
+            label_result.Text = result.ToString();
+
+        }
+
+        protected void button_times_Click(object sender, EventArgs e)
+        {
+
+            double value_one = double.Parse(value_one_TB.Text);
+            double value_two = double.Parse(value_two_TB.Text);
+
+            double result = value_one * value_two;
+
+            label_value_one.Text = value_one.ToString();
+            label_value_two.Text = value_two.ToString();
+            label_eqs.Text = " = ";
+
+            label_operator.Text = "  x  ";
+
+            label_result.Text = result.ToString();
+
+        }
+
+        protected void button_div_Click(object sender, EventArgs e)
+        {
+
+            double value_one = double.Parse(value_one_TB.Text);
+            double value_two = double.Parse(value_two_TB.Text);
+
+            double result = value_one / value_two;
+
+            label_value_one.Text = value_one.ToString();
+            label_value_two.Text = value_two.ToString();
+            label_eqs.Text = " = ";
+
+            label_operator.Text = "  ÷  ";
+
+            label_result.Text = result.ToString();
+
+        }
+    }
+}
